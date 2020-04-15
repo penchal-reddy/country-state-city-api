85dac1d5b22f504f0349d552127fc8501c67749d
penchal-reddy <penchal.reddy@imcsgroup.net>
2020-04-15 10:24:59 -0500

    added filters logic

--- src/main/java/net/imcs/app/repository/EmployeeRepository.java
+++ src/main/java/net/imcs/app/repository/EmployeeRepository.java
@@ -2,12 +2,12 @@ package net.imcs.app.repository;
 
 import java.util.List;
 
-import org.hibernate.criterion.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import net.imcs.app.entity.Employees;
 
-public interface EmployeeRepository extends JpaRepository<Employees,Long>{
+public interface EmployeeRepository extends JpaRepository<Employees,Long>,JpaSpecificationExecutor<Employees>{
 	
 	List<Employees> findByfirstName(String empName);
 	
@@ -30,6 +30,9 @@ public interface EmployeeRepository extends JpaRepository<Employees,Long>{
 	//@Query("Select e from employees e   left join  immigration i on i.emp_id=e.id left join visa_types v on v.visa_id=i.visa_id") 
 	//
 	
-	List<Employees> findByOrderByLastNameAsc();
+	//List<Employees> findByOrderByLastNameAsc();
+	
+	
+	
 
 }
